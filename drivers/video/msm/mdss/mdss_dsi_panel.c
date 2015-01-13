8f92ec4690658d59c59a655194c386c9e0c1cb60
YoshiShaPow <yoshipga@gmail.com>
2015-01-12 23:28:31 -0800

    drivers/video/msm/mdss/mdss_dsi_panel.c: add powersuspend hooks

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -32,6 +32,10 @@
 #include "mdss_dsi.h"
 #include "dsi_v2.h"
 
+#ifdef CONFIG_POWERSUSPEND
+#include <linux/powersuspend.h>
+#endif
+
 #define DT_CMD_HDR 6
 #define ESD_DROPBOX_MSG "ESD event detected"
 #define ESD_TE_DROPBOX_MSG "ESD TE event detected"
@@ -265,6 +269,10 @@ static int mdss_dsi_panel_partial_update(struct mdss_panel_data *pdata)
 		pr_err("%s: Invalid input data\n", __func__);
 		return -EINVAL;
 	}
+
+#ifdef CONFIG_POWERSUSPEND
+	set_power_suspend_state_panel_hook(POWER_SUSPEND_INACTIVE);
+#endif
 
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
@@ -663,6 +671,10 @@ disable_regs:
 	mdss_dsi_panel_regulator_on(pdata, 0);
 
 	pr_info("%s-:\n", __func__);
+
+#ifdef CONFIG_POWERSUSPEND
+	set_power_suspend_state_panel_hook(POWER_SUSPEND_ACTIVE);
+#endif
 
 	return 0;
 }
